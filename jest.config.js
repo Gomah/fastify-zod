a69798e7a05c25ce2cfd3d63e6b84c0196b62fc3
Gomah <hi@gomah.fr>
2022-06-10 13:33:42 +1000

    test: use ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  preset: `ts-jest`,
   testEnvironment: `node`,
-  testMatch: [`**/*.test.js`],
+  testMatch: [`**/*.test.ts`],
   maxConcurrency: 30,
   testTimeout: 240000,
 };
